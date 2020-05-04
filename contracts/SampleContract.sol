16294a0c35af6e25cdaeac9b948a95f72a942524
m-yahya <yahya_gis@hotmail.com>
2020-05-04 07:27:12 +0200

    Apply formatting and add event

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -1,13 +1,17 @@
 pragma solidity >=0.4.16 <0.6.0;
 
+
 contract SampleContract {
     uint256 data;
 
+    event NewData(address sender, uint256 data);
+
     function setData(uint256 x) public {
         data = x;
+        emit NewData(msg.sender, x);
     }
 
-    function getData() public view returns (uint256){
+    function getData() public view returns (uint256) {
         return data;
     }
 }
