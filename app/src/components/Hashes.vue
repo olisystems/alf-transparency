26fb3b03c82a8d5d920d33cce3ee0a9a9cbb3944
m-yahya <yahya_gis@hotmail.com>
2020-05-21 09:17:05 +0500

    CSELLS-25: Create Hashes component

---
+++ app/src/components/Hashes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+import web3 from "@/assets/js/web3";
+import alfTransparencyContract from "@/assets/js/contractInstance";
+
+export default {
+  name: "Hashes",
+
+  data() {
+    return {
+      account: "",
+      contract: "",
+    };
+  },
+
+  methods: {
+    getMetamaskAccount() {
+      web3.eth.getAccounts((err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this.account = res[0];
+      });
+    },
+  },
+
+  async created() {
+    this.getMetamaskAccount();
+    this.contract = await alfTransparencyContract();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
