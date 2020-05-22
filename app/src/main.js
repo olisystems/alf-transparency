d89d35d992a16790480244eb1a439e51ae879eab
m-yahya <yahya_gis@hotmail.com>
2020-05-22 14:21:21 +0500

    CSELLS-26: Add vue js smart table

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,14 @@
 import "v-tooltip/dist/v-tooltip.css";
 
 import App from "./App.vue";
+import SmartTable from "vuejs-smart-table";
 import VTooltip from "v-tooltip";
 import Vue from "vue";
 
+Vue.use(SmartTable);
 Vue.config.productionTip = false;
 Vue.use(VTooltip);
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
