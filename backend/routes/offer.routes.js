ea78fc1c21e04f12155ab29b690fe970b8286ea3
m-yahya <yahya_gis@hotmail.com>
2020-08-11 10:57:48 +0500

    AH-16: Add route comments

--- backend/routes/offer.routes.js
+++ backend/routes/offer.routes.js
@@ -2,13 +2,16 @@ module.exports = (app) => {
   const offers = require('../controllers/offer.controller.js')
   const router = require('express').Router()
 
+  // Create new Offer route
   // @params: Raw JSON { username: 'string', date: 'string', offer: 'string' }
   router.post('/', offers.create)
 
   // --- Demonstration routes --
-  // @params: -
+  // Retrieve all Offers route
   router.get('/all', offers.findAll)
+  // Find Offer by username
   router.get('/byUsername', offers.findByUsername)
+  // Find Offer by username and date
   router.get('/byUsernameDate', offers.findByUsernameDate)
 
   // --- Proof of Concept routes --
