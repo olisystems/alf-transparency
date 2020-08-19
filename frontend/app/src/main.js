2cbafebe7833643802229a221af8859199bfbd5e
m-yahya <yahya_gis@hotmail.com>
2020-08-19 11:54:23 +0200

    AH-35: Import routes

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,14 +1,21 @@
-import "v-tooltip/dist/v-tooltip.css";
+import 'v-tooltip/dist/v-tooltip.css'
 
-import App from "./App.vue";
-import SmartTable from "vuejs-smart-table";
-import VTooltip from "v-tooltip";
-import Vue from "vue";
+import App from './App.vue'
+import SmartTable from 'vuejs-smart-table'
+import VTooltip from 'v-tooltip'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
 
-Vue.use(SmartTable);
-Vue.config.productionTip = false;
-Vue.use(VTooltip);
+import './assets/css/navbar.css'
 
+Vue.use(SmartTable)
+Vue.config.productionTip = false
+Vue.use(VTooltip)
+Vue.use(VueRouter)
+
+const router = new VueRouter({ mode: 'history', routes })
 new Vue({
+  router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
