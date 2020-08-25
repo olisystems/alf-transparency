b9c2635a8fb9239d97ae8d20324fce39341c717a
m-yahya <yahya_gis@hotmail.com>
2020-08-25 18:21:02 +0200

    AH-40: Update server port

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 require('./routes/offer.routes')(app)
 
 // Set port, listen for requests
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
