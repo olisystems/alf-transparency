69e2e88f5948f5ccb68fbafc4223923dd535882d
m-yahya <yahya_gis@hotmail.com>
2020-08-26 14:48:03 +0200

    AH-45: Set up web3 provider

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -1,23 +1,7 @@
 /* eslint-disable */
-import Web3 from 'web3';
+import Web3 from 'web3'
 
-// Find or Inject Web3 Provider
-// Modern dapp browsers...
-if (window.ethereum) {
-    window.web3 = new Web3(ethereum);
-    try {
-        // Request account access
-        window.ethereum.enable();
-    } catch (error) {
-        // User denied account access...
-        console.error("User denied account access")
-    }
-    // Legacy dapp browsers...
-} else if (window.web3) {
-    window.web3 = new Web3(web3.currentProvider);
-} else {
-    // Non-dapp browsers...
-    console.error('Non-Ethereum browser detected. You should consider trying MetaMask!')
-}
+// const web3 = new Web3('ws://80.158.47.134:7546')
+const web3 = new Web3('ws://localhost:8545')
 
-export default web3;
+export default web3
