26cef980914e3d12dfbbbdef65ea23ac7159bfaa
m-yahya <yahya_gis@hotmail.com>
2020-08-26 14:47:31 +0200

    AH-44: Apply code formatting

--- app/src/assets/js/contractInstance.js
+++ app/src/assets/js/contractInstance.js
@@ -1,18 +1,18 @@
-import alfTransparencyArtifact from "./contracts/ALFTransparency";
-import web3 from "./web3";
+import alfTransparencyArtifact from './contracts/ALFTransparency'
+import web3 from './web3'
 
-const alfTransparencyContract = async () => {
+const Contract = async () => {
   //This method find the network id to retrieve the configuration from truffle-config.js file
-  const networkId = await web3.eth.net.getId();
+  const networkId = await web3.eth.net.getId()
   // Retrieve the Network configuration from truffle-config.js file
-  const deployedNetwork = alfTransparencyArtifact.networks[networkId];
+  const deployedNetwork = alfTransparencyArtifact.networks[networkId]
   // Initializing the contract
   const ALFTransparency = new web3.eth.Contract(
     alfTransparencyArtifact.abi,
-    deployedNetwork.address
-  );
+    deployedNetwork.address,
+  )
 
-  return ALFTransparency;
-};
+  return ALFTransparency
+}
 
-export default alfTransparencyContract;
+export default Contract
