0d3ea70f513e81cbc0415c4bdc680a9ba21f83e1
Muhammad Yahya <yahya_gis@hotmail.com>
2020-09-11 12:12:09 +0200

    AH-30: Implement Solidity timestamp

--- truffle/test/alfTransparency.js
+++ truffle/test/alfTransparency.js
@@ -7,8 +7,7 @@ contract('ALFTransparency', function (accounts) {
   let user = accounts[1]
   let alfTransparencyContract
   let hash = '7b1a04da83bfcaa8cb98d1cf8189b1a7cc5bea96e6c5022e205ff8c0ee43a6ff'
-  let username = 'John Doe'
-  let timestamp = '1596691896'
+  let date = '2020-09-11'
 
   // create global contract instance before executing tests
   before('setup contract', async () => {
@@ -27,15 +26,14 @@ contract('ALFTransparency', function (accounts) {
     // set data value
     const result = await alfTransparencyContract.sendHash(
       hash,
-      username,
-      timestamp,
+      date,
       { from: owner },
     )
 
     // test event
     truffleAssert.eventEmitted(result, 'NewHash', (ev) => {
       return (
-        ev.rootHash == hash && ev.user == username && ev.timestamp == timestamp
+        ev.rootHash == hash && ev.date == date
       )
     })
   })
@@ -43,7 +41,7 @@ contract('ALFTransparency', function (accounts) {
   // test 3
   it('should fail to send the hash with the same timestamp', async () => {
     await truffleAssert.reverts(
-      alfTransparencyContract.sendHash(hash, username, timestamp, {
+      alfTransparencyContract.sendHash(hash, date, {
         from: owner,
       }),
     )
@@ -52,17 +50,16 @@ contract('ALFTransparency', function (accounts) {
   // test 4
   it('should fail to send the hash from the user account', async () => {
     await truffleAssert.reverts(
-      alfTransparencyContract.sendHash(hash, username, timestamp, {
+      alfTransparencyContract.sendHash(hash, date, {
         from: user,
       }),
     )
   })
 
   // test 5
-  it('should return hash and username', async () => {
+  it('should return hash and date', async () => {
     // set data value
-    const result = await alfTransparencyContract.getHash.call(timestamp)
+    const result = await alfTransparencyContract.getHash.call(date)
     assert.equal(result[0], hash, 'Failed to get correct hash')
-    assert.equal(result[1], username, 'Failed to get correct username')
   })
 })
