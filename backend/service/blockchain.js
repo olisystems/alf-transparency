3f1920c5527de50d1e4512b88f88010f1619d85e
Muhammad Yahya <yahya_gis@hotmail.com>
2020-09-28 10:42:27 +0200

    AH-59: add WSS url

--- backend/service/blockchain.js
+++ backend/service/blockchain.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3')
 const { ADDRESS, ABI } = require('./metadata')
 const web3 = new Web3('ws://80.158.47.134:7546')
-// const web3 = new Web3('ws://localhost:8545')
+// const web3 = new Web3('wss://volta-blockchain.my-oli.com')
 
 const Contract = new web3.eth.Contract(ABI, ADDRESS)
 module.exports = Contract
