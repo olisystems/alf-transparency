47edf543fcac9e18f6d8b2c56492cd43873f91a8
Muhammad Yahya <yahya_gis@hotmail.com>
2020-09-28 10:47:43 +0200

    AH-59: add WSS URL to connect with Volta node

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -2,6 +2,6 @@
 import Web3 from "web3";
 
 const web3 = new Web3("wss://volta-blockchain.my-oli.com");
-// const web3 = new Web3('ws://localhost:8545')
+//const web3 = new Web3('ws://localhost:8545')
 
 export default web3;
