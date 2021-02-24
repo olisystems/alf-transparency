489766d6108361a271f63fd9081b7ae0211a63bd
m-yahya <yahya_gis@hotmail.com>
2021-02-24 03:22:53 +0100

    Connect to webserver volta node

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -1,7 +1,33 @@
 /* eslint-disable */
 import Web3 from "web3";
 
-const web3 = new Web3("wss://volta-blockchain.my-oli.com");
-//const web3 = new Web3('ws://localhost:8545')
+const VOLTA_NODE = "wss://volta-blockchain.my-oli.com";
+var provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
+var web3 = new Web3(provider);
+
+// on connect
+provider.on("connect", () => console.log("WS Connected"));
+
+// on error
+provider.on("error", () => {
+  console.log("WS Error");
+  console.log("Attempting to reconnect...");
+  provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
+
+  provider.on("connect", () => console.log("WS Re-connected"));
+
+  web3.setProvider(provider);
+});
+
+// on connection close
+provider.on("end", () => {
+  console.log("WS closed");
+  console.log("Attempting to reconnect...");
+  provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
+
+  provider.on("connect", () => console.log("WS Re-connected"));
+
+  web3.setProvider(provider);
+});
 
 export default web3;
